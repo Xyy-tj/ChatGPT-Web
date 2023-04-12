6d408b5fb296fbddc47cf8d33f9f88f8389079ed
Fan Zhaoyu <zhaoyu.fan@foxmail.com>
2023-04-12 11:13:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import { NConfigProvider } from 'naive-ui'
 import { NaiveProvider } from '@/components/common'
 import { useTheme } from '@/hooks/useTheme'
 import { useLanguage } from '@/hooks/useLanguage'
+import { onMounted } from 'vue'
+
 
 const { theme, themeOverrides } = useTheme()
 const { language } = useLanguage()
-
-import { onMounted } from 'vue'
 
 onMounted(() => {
   window.alert('asdasd')
