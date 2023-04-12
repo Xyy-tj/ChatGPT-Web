003dcafa5dabeaf2e35eec3379f584f26a4ae52c
Fan Zhaoyu <zhaoyu.fan@foxmail.com>
2023-04-12 11:32:10 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@ import { NConfigProvider } from 'naive-ui'
 import { NaiveProvider } from '@/components/common'
 import { useTheme } from '@/hooks/useTheme'
 import { useLanguage } from '@/hooks/useLanguage'
+import { onMounted } from 'vue'
 
 const { theme, themeOverrides } = useTheme()
 const { language } = useLanguage()
-
-import { onMounted } from 'vue'
 
 onMounted(() => {
   window.alert('请及时重置CatGPT对话窗口清理历史信息（输入框左侧垃圾箱小图标），以减小对话token消耗，CatGPT表示感谢！')
